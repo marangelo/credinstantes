88e4dd4d707a03366e417be1cf7fefde16ea3fee
Ing. Maryan Espinoza <ANALISTA.GUMA@GMAIL.COM>
2023-11-24 15:09:54 -0600

    Refact: ReCalcEstados

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -13,43 +13,30 @@ class ApiController extends Controller{
     {  
 
         // php artisan run:CalcularEstadosCredito
-        $Creditos_mora    = [];
-        $Creditos_venc    = [];
-        $Creditos_merg    = [];
-        $idEstado         = 1; 
-        $DsVenc           = 0;
+        $Creditos         = [];
+        $Estado           = 1 ;
         $batch_Credito    = new Credito;
         $batch_index      = 'id_creditos';
 
-        $Creditos_Estados = EstadosMonitor::Where('SALDO_CREDITO', '>', 0)->Where('CLIENTE_ACTIVO', $idEstado)->get();            
+        $Creditos_Estados = EstadosMonitor::Where('SALDO_CREDITO', '>', 0)->Where('CLIENTE_ACTIVO', 1)->get();            
 
         foreach ($Creditos_Estados as $key => $c) {
-            if ($c->VENCIDO === 'S' ) {
-                $Creditos_venc[$key] = [
-                    'id_creditos'       => $c->ID_CREDITO,
-                    'estado_credito'    => 3
-                ];
-            }else {
-                if ($c->MORA === 'S' ) {
-                    $Creditos_mora[$key] = [
-                        'id_creditos'       => $c->ID_CREDITO,
-                        'estado_credito'    => 2
-                    ];
-                }
-            }
+            $Estado = ($c->MORA == 'S') ? 2 : $Estado ;
+            $Estado = ($c->VENCIDO == 'S') ? 3 : $Estado ;
+            $Creditos[$key] = [
+                'id_creditos'       => $c->ID_CREDITO,
+                'estado_credito'    => $Estado
+            ];
 
         }
 
-        $array_merge = array_merge($Creditos_mora, $Creditos_venc);
+      
 
-
-
-        \Batch::update($batch_Credito, $Creditos_mora, $batch_index);        
-        \Batch::update($batch_Credito, $Creditos_venc, $batch_index);
+        \Batch::update($batch_Credito, $Creditos, $batch_index);
 
         \Log::channel('log_calc_Estados')->info("Se calculo los estados para el dia de  ".date('Y-m-d H:i:s'));
 
-        return response()->json($array_merge);
+        return response()->json($Creditos);
 
     }
 }
