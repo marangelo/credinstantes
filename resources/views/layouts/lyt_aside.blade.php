f469a24016da8b46a40dfb85ea099f728667a100
Guma Desarrollo <ANALISTA.GUMA@GMAIL.COM>
2023-12-15 21:58:43 -0600

    Fix: Saldos de Cartera en Dashboard

--- resources/views/layouts/lyt_aside.blade.php
+++ resources/views/layouts/lyt_aside.blade.php
@@ -22,7 +22,7 @@
   <!-- /.navbar -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="!#" class="brand-link">
+    <a href="#!" class="brand-link">
       <img src="{{ asset('img/Logo.png') }}" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">CREDIN$TANTES</span>
     </a>
