3db0725f69b558dde5e62c4a7460ec3891a9f034
Guma Desarrollo <ANALISTA.GUMA@GMAIL.COM>
2025-01-23 11:08:22 -0600

    🔥 [FIX] Nombre de archivo de logo

--- resources/views/layouts/lyt_aside.blade.php
+++ resources/views/layouts/lyt_aside.blade.php
@@ -23,7 +23,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#!" class="brand-link">
-      <img src="{{ asset('img/Logo.png') }}" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('img/logo.png') }}" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">CREDIN$TANTE</span>
     </a>
 
