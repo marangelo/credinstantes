b58f76ee4cf81b76cb615c051eb895932b767c0b
Guma Desarrollo <ANALISTA.GUMA@GMAIL.COM>
2025-06-03 11:12:56 -0600

    ✅ [ADD] UI Fiador

--- resources/views/Solicitudes/RequestRenovacion.blade.php
+++ resources/views/Solicitudes/RequestRenovacion.blade.php
@@ -100,11 +100,14 @@
                               <li class="nav-item">
                                   <a class="nav-link" id="custom-tabs-one-settings-tab" data-toggle="pill" href="#custom-tabs-one-settings" role="tab" aria-controls="custom-tabs-one-settings" aria-selected="false">DETALLES DE GARANTIAS</a>
                               </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" id="custom-tabs-fiador-tab" data-toggle="pill" href="#custom-tabs-fiador" role="tab" aria-controls="custom-tabs-fiador" aria-selected="false">DETALLES DE FIADOR</a>
+                              </li>
                           </ul>
                         </div>
                         <div class="card-body">
                           <div class="tab-content" id="custom-tabs-one-tabContent">
-                            <div class="tab-pane fade show active" id="custom-tabs-one-home" role="tabpanel" aria-labelledby="custom-tabs-one-home-tab">
+                              <div class="tab-pane fade show active" id="custom-tabs-one-home" role="tabpanel" aria-labelledby="custom-tabs-one-home-tab">
                                 <div class="row gx-2">
                                   <div class="col-sm-6 col-md-6 col-md-4 col-xl-3">
                   
@@ -412,6 +415,71 @@
                                       </div>
                                   </div>
                                       
+                              </div>
+
+                              <div class="tab-pane fade" id="custom-tabs-fiador" role="tabpanel" aria-labelledby="ref-fiador-tab">
+                                  <form action="#!" method="post" id="frm_info_conyugue">                                
+                                    <div class="row gx-2">
+                                        <div class="col-sm-6 mb-3">
+                                            <label class="form-label" for="event-name">Nombres:</label>
+                                            <div class="input-group"><span class="input-group-text "><span class="fas fa-user"></span></span>
+                                                <input class="form-control" type="text" name="nombres_fiador" placeholder="Nombres de la persona" required="" value="" />
+                                            </div>
+                                        </div>
+
+                                        <div class="col-sm-6 mb-3">
+                                            <label class="form-label" for="event-name">Apellidos:</label>
+                                            <div class="input-group"><span class="input-group-text "><span class="fas fa-user"></span></span>
+                                                <input class="form-control" type="text" name="apellidos_fiador" placeholder="Apellidos de la persona" required="" value="" />
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-sm-6 col-md-4 mb-3">
+                                            <label class="form-label" for="event-name">Cedula:</label>
+                                            <div class="input-group"><span class="input-group-text "><span class="far fa-address-card"></span></span>
+                                                <input class="form-control" id="event-name" type="text" name="cedula_fiador" placeholder="000-000000-0000A" 
+                                                data-inputmask="'mask': ['999-999999-9999A']" data-mask required="" value=""/>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-4 mb-3">
+                                            <label class="form-label" for="event-name">Estado Civil</label>
+                                            <select class="custom-select" name="selectEstadoCivil" id="selectEstadoCivil">
+                                                <option value="0" {{ ($Cliente->estado_civil ?? '') == '0' ? 'selected' : '' }}>Seleccione un estado civil</option>
+                                                <option value="1" {{ ($Cliente->estado_civil ?? '') == '1' ? 'selected' : '' }}>Soltero(a)</option>
+                                                <option value="2" {{ ($Cliente->estado_civil ?? '') == '2' ? 'selected' : '' }}>Casado(a)</option>
+                                                <option value="3" {{ ($Cliente->estado_civil ?? '') == '3' ? 'selected' : '' }}>Divorciado(a)</option>
+                                                <option value="4" {{ ($Cliente->estado_civil ?? '') == '4' ? 'selected' : '' }}>Viudo(a)</option>
+                                                <option value="5" {{ ($Cliente->estado_civil ?? '') == '5' ? 'selected' : '' }}>Union Libre</option>
+                                            </select>
+                                        </div>
+
+                                        <div class="col-sm-6 col-md-4 mb-3">
+                                            <label class="form-label" for="event-name">Teléfono:</label>
+                                            <div class="input-group"><span class="input-group-text "><span class="fas fa-phone-alt"></span></span>
+                                                <input class="form-control" id="event-name" type="text" name="telefono_fiador" onkeypress='return isNumberKey(event)' maxlength="8" value="" />
+                                            </div>
+                                        </div>
+
+                                        <div class="col-12">
+                                            <div class="row">
+                                                <div class="col-12">
+                                                    <label class="form-label" for="event-description">Dirección  de domilio:</label>
+                                                    <textarea class="form-control" rows="6" required="" name="dir_domicilio_fiador" ></textarea>
+                                                </div>                                                
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="col-12">
+                                            <div class="row">
+                                                <div class="col-12">
+                                                    <label class="form-label" for="event-description">Lugar de trabajo:</label>
+                                                    <textarea class="form-control" rows="6" required="" name="dir_trabajo_fiador" ></textarea>
+                                                </div>                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
                               </div>
                               
 
